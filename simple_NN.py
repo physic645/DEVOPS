f39818ca4caf1bc744d92cf1af83568be77e9b82
Konstantinos <kofilipp@physics.auth.gr>
2022-12-13 22:09:56 +0200

    keras example model

--- simple_NN.py
+++ simple_NN.py
@@ -1,5 +1,5 @@
 # A simple NN using tensorflow
-
+'''
 import keras
 import keras.optimizers
 from keras.models                import Sequential
@@ -15,3 +15,26 @@ from keras.callbacks             import LambdaCallback
 from keras.utils.vis_utils       import plot_model # for visualization the model
 
 from sklearn.model_selection     import cross_val_score
+'''
+
+# first neural network with keras tutorial
+from numpy import loadtxt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+# load the dataset
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_shape=(8,), activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit the keras model on the dataset
+model.fit(X, y, epochs=15, batch_size=10)
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
