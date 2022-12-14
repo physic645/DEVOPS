de9cafcb7e17523dba97126f13b249b4021c28f1
Konstantinos <kofilipp@physics.auth.gr>
2022-12-14 23:46:30 +0200

    generic input combinations

--- python_runner.py
+++ python_runner.py
@@ -3,24 +3,36 @@
 # f.write(f'i = {i} \n')
 from simple_NN import simple_NN
 import time
+from itertools import product
 
 start = time.time()
 
-# Create a sequence of numbers of nodes for first layer 10 until 30 with step 5
+# Define a list of parameters that we want to run
+nodes_of_first_layer = [10,20,30]
+epochs               = [1,10]
+
 f = open("demo_results.txt", "w")
-f.write('i \t')
+f.write('nodes1st-epoch \t')
 f.write('accuracy \t')
 f.write('loss \t')
 f.write('\n')
-epochs = 10000
-for i in range(10,31,10):
+
+i=0
+
+# Create a sequence of numbers of nodes 
+# for number of first layer nodes here 10 until 30 with step 10 i.e 10,20,30
+# product -> is Cartesian product =>
+# takes all the possible combinatios of the input list
+for args in product(nodes_of_first_layer,epochs):
     
-    # Run the neural network with different first layer parameters
-    loss,accuracy = simple_NN(i,epochs)    
-    f.write(f'{i} \t')
-    f.write(f'{accuracy*100:.3f} \t')
+    # Run the neural network with different first layer and epochs parameters
+    # We create all the possible combinations here 10,1 / 10,2 / 20,1 / 20,2
+    loss,accuracy = simple_NN(*args)        
+    f.write(f'[{args[i]}-{args[i+1]}] \t\t\t\t')
+    f.write(f'{accuracy*100:.3f} \t\t')
     f.write(f'{loss*100:.3f} \t')
     f.write('\n')
+    
     #f.write('Loss: %.2f\n' % (loss*100))
     
 end = time.time()
