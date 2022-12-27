b83d99da1f945aa72b5045f723df4553e4d45bd3
Filippou Konstantinos <kofilipp@physics.auth.gr>
2022-12-27 21:27:43 +0200

    add pair t-test

--- python_runner.py
+++ python_runner.py
@@ -9,11 +9,11 @@ import time
 start = time.time()
 
 # Define a list of parameters that we want to run
-nodes_layer_1_scenario_1      = [128]
-epochs_scenario_1             = [10000]
+nodes_layer_1_scenario_1      = [64,128]
+epochs_scenario_1             = [1]
 
-nodes_layer_1_scenario_2      = [64]
-epochs_scenario_2             = [10000]
+nodes_layer_1_scenario_2      = [64,128]
+epochs_scenario_2             = [100]
 
 searchname                    = "heart"
 
@@ -21,18 +21,18 @@ searchname                    = "heart"
 X,y,input_shape = p_kaggle(searchname)
 
 
-loss_all_scenario_1 = run_NN(nodes_layer_1_scenario_1,epochs_scenario_1,X,y,input_shape)
+accuracy_1,loss_all_scenario_1 = run_NN(nodes_layer_1_scenario_1,epochs_scenario_1,X,y,input_shape)
 
-loss_all_scenario_2 = run_NN(nodes_layer_1_scenario_2,epochs_scenario_2,X,y,input_shape)
+accuracy_2,loss_all_scenario_2 = run_NN(nodes_layer_1_scenario_2,epochs_scenario_2,X,y,input_shape)
 
 
-# Perform t-test for losses for the two scenarios
-t_statistic, p_value = stats.ttest_ind(loss_all_scenario_1, loss_all_scenario_2)
+# Perform paired t-test for losses for the two scenarios
+t_statistic, p_value = stats.ttest_rel(loss_all_scenario_1, loss_all_scenario_2)
 
 
 # Print results
 print(f'T-statistic: {t_statistic:.5f} \n')
-print(f'P-value    : {p_value:.5f}\n')
+print(f'P-value    : {p_value:.5f}     \n')
 
 
 end = time.time()
