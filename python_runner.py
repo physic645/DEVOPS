838e76fdb2a43395f934b166efeb295f059ebe2f
Filippou Konstantinos <kofilipp@physics.auth.gr>
2022-12-28 20:16:46 +0200

    edit runner

--- python_runner.py
+++ python_runner.py
@@ -10,30 +10,26 @@ start = time.time()
 
 # Define a list of parameters that we want to run
 nodes_layer_1_scenario_1      = [64,128]
-epochs_scenario_1             = [100000]
+epochs_scenario_1             = [1]
 
 nodes_layer_1_scenario_2      = [64,128]
-epochs_scenario_2             = [1000000]
+epochs_scenario_2             = [10]
 
 searchname                    = "heart"
 
 # Call p_kaggle and download the working dataset once
 X,y,input_shape = p_kaggle(searchname)
 
-
 accuracy_1,loss_all_scenario_1 = run_NN(nodes_layer_1_scenario_1,epochs_scenario_1,X,y,input_shape)
 
 accuracy_2,loss_all_scenario_2 = run_NN(nodes_layer_1_scenario_2,epochs_scenario_2,X,y,input_shape)
 
-
 # Perform paired t-test for losses for the two scenarios
 t_statistic, p_value = stats.ttest_rel(loss_all_scenario_1, loss_all_scenario_2)
-
 
 # Print results
 print(f'T-statistic: {t_statistic:.5f} \n')
 print(f'P-value    : {p_value:.5f}     \n')
-
 
 end = time.time()
 total = end-start
