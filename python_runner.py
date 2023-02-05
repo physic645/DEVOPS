235cef552b15d54718c35621daee89cc93443649
Konstantinos <kofilipp@physics.auth.gr>
2023-02-05 10:42:03 +0200

    diabetes

--- python_runner.py
+++ python_runner.py
@@ -22,8 +22,8 @@ start_program_time = time.time()
 
 # Step 1: Connect_with_kaggle and download the working dataset onceee
 
-#searchname      = "diabetes"
-searchname       = "heart attack"
+
+searchname       = "diabetes"
 X,y,input_shape = connect_with_kaggle(searchname)
 
 
